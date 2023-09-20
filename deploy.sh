f503a9e245723b6718ee7020e7d0695340ec829b
Jasper <well.caffeinated@gmail.com>
2023-09-19 20:01:49 -0600

    fix build

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+REPO="eclipses.git"
+
+# abort on errors
+set -e
+
+# build
+bun run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git branch -m main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:minutelabsio/$REPO main:gh-pages
+
+cd -
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: process.env.NODE_ENV === 'production'
+    ? `/${pkg.name}/`
+    : '/'
 })
