ecae595ed98f90caf02f128f46bc11d4a28692d5
Jasper <well.caffeinated@gmail.com>
2024-03-28 13:58:51 -0600

    replace noise function to fix streaks bug

--- src/shaders/sky/fbm.glsl
+++ src/shaders/sky/fbm.glsl
@@ -1,42 +1,99 @@
-#define NUM_OCTAVES 3
+// By Morgan McGuire @morgan3d, http://graphicscodex.com
+// Reuse permitted under the BSD license.
 
-float mod289(float x) {
-  return x - floor(x * (1.0 / 289.0)) * 289.0;
+// All noise functions are designed for values on integer scale.
+// They are tuned to avoid visible periodicity for both positive and
+// negative coordinates within a few orders of magnitude.
+
+#define NOISE fbm
+#define NUM_NOISE_OCTAVES 3
+
+// Precision-adjusted variations of https://www.shadertoy.com/view/4djSRW
+float hash(float p) {
+  p = fract(p * 0.011);
+  p *= p + 7.5;
+  p *= p + p;
+  return fract(p);
 }
-vec4 mod289(vec4 x) {
-  return x - floor(x * (1.0 / 289.0)) * 289.0;
+float hash(vec2 p) {
+  vec3 p3 = fract(vec3(p.xyx) * 0.13);
+  p3 += dot(p3, p3.yzx + 3.333);
+  return fract((p3.x + p3.y) * p3.z);
 }
-vec4 perm(vec4 x) {
-  return mod289(((x * 34.0) + 1.0) * x);
+
+float noise(float x) {
+  float i = floor(x);
+  float f = fract(x);
+  float u = f * f * (3.0 - 2.0 * f);
+  return mix(hash(i), hash(i + 1.0), u);
 }
 
-float noise(vec3 p) {
-  vec3 a = floor(p);
-  vec3 d = p - a;
-  d = d * d * (3.0 - 2.0 * d);
+float noise(vec2 x) {
+  vec2 i = floor(x);
+  vec2 f = fract(x);
 
-  vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
-  vec4 k1 = perm(b.xyxy);
-  vec4 k2 = perm(k1.xyxy + b.zzww);
+  // Four corners in 2D of a tile
+  float a = hash(i);
+  float b = hash(i + vec2(1.0, 0.0));
+  float c = hash(i + vec2(0.0, 1.0));
+  float d = hash(i + vec2(1.0, 1.0));
 
-  vec4 c = k2 + a.zzzz;
-  vec4 k3 = perm(c);
-  vec4 k4 = perm(c + 1.0);
+  // Simple 2D lerp using smoothstep envelope between the values.
+  // return vec3(mix(mix(a, b, smoothstep(0.0, 1.0, f.x)),
+  //      mix(c, d, smoothstep(0.0, 1.0, f.x)),
+  //      smoothstep(0.0, 1.0, f.y)));
 
-  vec4 o1 = fract(k3 * (1.0 / 41.0));
-  vec4 o2 = fract(k4 * (1.0 / 41.0));
+  // Same code, with the clamps in smoothstep and common subexpressions
+  // optimized away.
+  vec2 u = f * f * (3.0 - 2.0 * f);
+  return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+}
 
-  vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
-  vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
+float noise(vec3 x) {
+  const vec3 step = vec3(110, 241, 171);
 
-  return o4.y * d.y + o4.x * (1.0 - d.y);
+  vec3 i = floor(x);
+  vec3 f = fract(x);
+
+    // For performance, compute the base input to a 1D hash from the integer part of the argument and the
+    // incremental change to the 1D based on the 3D -> 1D wrapping
+  float n = dot(i, step);
+
+  vec3 u = f * f * (3.0 - 2.0 * f);
+  return mix(mix(mix(hash(n + dot(step, vec3(0, 0, 0))), hash(n + dot(step, vec3(1, 0, 0))), u.x), mix(hash(n + dot(step, vec3(0, 1, 0))), hash(n + dot(step, vec3(1, 1, 0))), u.x), u.y), mix(mix(hash(n + dot(step, vec3(0, 0, 1))), hash(n + dot(step, vec3(1, 0, 1))), u.x), mix(hash(n + dot(step, vec3(0, 1, 1))), hash(n + dot(step, vec3(1, 1, 1))), u.x), u.y), u.z);
+}
+
+float fbm(float x) {
+  float v = 0.0;
+  float a = 0.5;
+  float shift = float(100);
+  for(int i = 0; i < NUM_NOISE_OCTAVES; ++i) {
+    v += a * noise(x);
+    x = x * 2.0 + shift;
+    a *= 0.5;
+  }
+  return v;
+}
+
+float fbm(vec2 x) {
+  float v = 0.0;
+  float a = 0.5;
+  vec2 shift = vec2(100);
+  // Rotate to reduce axial bias
+  mat2 rot = mat2(cos(0.5), sin(0.5), -sin(0.5), cos(0.50));
+  for(int i = 0; i < NUM_NOISE_OCTAVES; ++i) {
+    v += a * noise(x);
+    x = rot * x * 2.0 + shift;
+    a *= 0.5;
+  }
+  return v;
 }
 
 float fbm(vec3 x) {
   float v = 0.0;
   float a = 0.5;
   vec3 shift = vec3(100);
-  for(int i = 0; i < NUM_OCTAVES; ++i) {
+  for(int i = 0; i < NUM_NOISE_OCTAVES; ++i) {
     v += a * noise(x);
     x = x * 2.0 + shift;
     a *= 0.5;
